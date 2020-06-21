955912a621876e10c7e06a055f55bea545a93ca4
Anna Geckler <annageckler@Vernons-iMac.local>
2020-06-20 18:22:14 -0600

    deleted logo files and added error boundry component

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry'
 import './app.css';
 
 
@@ -35,8 +36,10 @@ class App extends Component {
 			<div className='tc'>
 				<h1 className='f1'>RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
-				<Scroll height='800px'>
-					<CardList robots={filteredRobots}/>
+				<Scroll height='500px'>
+					<ErrorBoundry>
+						<CardList robots={filteredRobots}/>
+					</ErrorBoundry>
 				</Scroll>
 			</div>
 		);
